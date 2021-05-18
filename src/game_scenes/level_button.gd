42f114e237f57ddde75e12ec31efbd208817eb35
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2021-05-18 16:39:55 +0800

    improved level select scene design

--- src/game_scenes/level_button.gd
+++ src/game_scenes/level_button.gd
@@ -1,8 +1,17 @@
-extends Button
+extends TextureButton
 
 
 var level_path : String
 
-
+func _ready():
+	set_Label()
+	
+func set_Label():
+	$Label.text = name
+	
 func _on_Button_pressed():
+	get_tree().change_scene(level_path)
+
+
+func _on_TextureButton_pressed():
 	get_tree().change_scene(level_path)
--- src/game_scenes/level_select.gd
+++ src/game_scenes/level_select.gd
@@ -7,24 +7,24 @@ export (NodePath) var grid
 func _ready():
 	grid = get_node(grid)
 	
-	if total_levels > 4:
+	if total_levels > 15:
 		column_size()
 		
 	for i in range(0, total_levels):
 		generate_buttons(i + 1)
+		
 
 func generate_buttons(name : int):
 	var base = base_button.instance()
 	base.set_name(str(name))
-	base.set_text(str(name))
 	base.level_path = ("res://src/levels/level_" + str(name) + ".tscn")
 	grid.add_child(base)
 	
 func column_size():
-	if total_levels % 2 == 0:
-		grid.columns = total_levels / 2
+	if total_levels % 5 == 0:
+		grid.columns = total_levels / 3
 	else:
-		grid.columns = total_levels / 2 + 1
+		grid.columns = total_levels / 3 + 1
 
 func _on_Back_Button_pressed():
 		get_tree().change_scene("res://src/home_menu_screen.tscn")
