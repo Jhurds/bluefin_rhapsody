8ae838b9bfd97212c22a77bbf2f335c61ad2f5a2
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2021-05-18 21:35:09 +0800

    connected ui's and linked finished scenes

--- src/Buttons/title_screen.gd
+++ src/Buttons/title_screen.gd
@@ -16,3 +16,8 @@ func _on_Button_pressed(scene_to_load):
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path)
 	
+
+
+
+
+
---
+++ src/game_scenes/Options.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://src/home_menu_screen.tscn")
