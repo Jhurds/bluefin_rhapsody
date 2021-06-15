07ca448fb780a55828552f082e1b3f9df0dcc8fd
George <georgejoseabaygar@gmail.com>
2021-06-15 13:40:46 +0800

    added placeholder anchovy

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -4,7 +4,7 @@ class_name actor
 export var ACCELERATION: = 3000
 var velocity: = Vector2.ZERO
 export var MAX_SPEED: = 700
-export var FRICTION: = 2000
+export var FRICTION: = 2000 #of water
 var facing_right = true
 var screen_size = Vector2.ZERO
 
---
+++ src/Actors/anchovy_script.gd
@@ -0,0 +1,7 @@
+extends "res://src/actors/actor.gd"
+
+
+
+func _ready():
+	pass # Replace with function body.
+
