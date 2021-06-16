e12aa4fa01ab3d9dd4302cf117a1065128cbfc82
George <georgejoseabaygar@gmail.com>
2021-06-16 17:40:25 +0800

    added food pool in level 1

--- src/levels/level_1.gd
+++ src/levels/level_1.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+var food_pool = []
 onready var player = get_node("Player")
 
+func _on_foodDetector_body_entered(body):
+	food_pool.append(body)
+
+func _on_foodDetector_body_exited(body):
+	food_pool.erase(body)
 
 func _ready():
 	$level_cleared/Control.visible = false
@@ -14,4 +20,5 @@ func _process(_delta):
 		$level_cleared/Control/score_box/gained_calories.text = str(calories_gained)
 		$level_cleared/Control.visible = true
 		$level_cleared/Control.get_tree().paused = true
-		
+	print(food_pool.size())
+
