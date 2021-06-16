fb7454ee2267d55f2089acf5280050f21b1f7a91
George <georgejoseabaygar@gmail.com>
2021-06-16 11:55:19 +0800

    what

--- src/Actors/anchovy.gd
+++ src/Actors/anchovy.gd
@@ -2,19 +2,18 @@ extends actor
 class_name anchovy
 
 var starting_position
-export var patrol_range = 0
+
 
 func _ready():
 	_velocity.x = -MAX_SPEED
 	starting_position = position
 	scale.x *= -1.0
-	patrol_range = randi() % patrol_range + 100
 	
 func _on_eatDetector_body_entered(body):
 	queue_free()
 
 func _physics_process(_delta) -> void:
-	if position.x <= (starting_position.x - patrol_range) or position.x >= (starting_position.x + patrol_range):
+	if position.x <= (starting_position.x - 300) or position.x >= (starting_position.x + 300):
 		_velocity.x *= -1.0
 		scale.x *= -1.0
 	_velocity.y = move_and_slide(_velocity).y
