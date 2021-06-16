12a3bb31e54828f41f1c5dcd62e84b7f6623aedb
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2021-06-16 14:37:05 +0800

    changed score to calorie count

--- src/levels/level_1.gd
+++ src/levels/level_1.gd
@@ -5,9 +5,8 @@ onready var player = get_node("player")
 
 func _process(_delta):
 	print(player.eaten)
+	
 	if player.calories >= 2400:
-			$level_cleared/Control.visible = true
-			$level_cleared/Control.get_tree().paused = true
-	print(player.calories)
-	
-	
+		$level_cleared/Control/score_box/Label.text = str(player.calories)
+		$level_cleared/Control.visible = true
+		$level_cleared/Control.get_tree().paused = true
